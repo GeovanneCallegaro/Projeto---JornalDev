2d680901b28d60752027fd73924b95d0997bce3d
Geovanne <callegarog00@gmail.com>
2022-03-08 09:41:50 -0300

    authentication for admin done

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -1,8 +1,12 @@
 const checkIfAdminIsTrue = require("../helpers/verifyAdminUser")
+const checkToken = require("../helpers/verifyToken")
+const User = require("../models/User")
 
 
 module.exports = class AdminController {
     static async users(req, res) {
-        await checkIfAdminIsTrue(req, res)
+        const user = await User.find({ admin: false})
+        console.log('Ok')
+        
     }
 }
--- backend/helpers/verifyAdminUser.js
+++ backend/helpers/verifyAdminUser.js
@@ -3,24 +3,19 @@ const getToken = require('./getToken')
 const User = require('../models/User')
 
 const checkIfAdminIsTrue = async (req, res, next) => {
-    const token = getToken(req)
-    const decoded = jwt.verify(token, "secret")
-
-    const getUser = await User.findById(decoded.id)
+        const token = getToken(req)
+        const decoded = jwt.verify(token, "secret")
+        const getUser = await User.findById(decoded.id)
 
     if(!token) {
         return res.status(401).json({ message: "Acesso Negado"})
     }
 
-    try {
-        if(getUser.admin) {
-            res.status(200) .json({message: 'OK!'})
-        } else {
-            next()
-        }
-    } catch(err) {
-        return res.status(400).json({message: "Usuário não é admin!"})
-    }
+    if(getUser.admin !== true) {
+        return res.status(200) .json({message: 'Não é admin!'})
+    } 
+
+    next()
 }
 
 module.exports = checkIfAdminIsTrue
