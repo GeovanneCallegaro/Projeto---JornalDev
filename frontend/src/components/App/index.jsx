8939f2d50c93fdc0ad4fd56d3a321bc1df8dbaa0
Geovanne <callegarog00@gmail.com>
2022-03-11 17:53:39 -0300

    creating user context and sharing in application

--- frontend/src/components/App/index.jsx
+++ frontend/src/components/App/index.jsx
@@ -3,17 +3,21 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { Home } from "../Home";
 import { Login } from '../Auth/Login'
 
+import {UserProvider} from '../../context/userContext'
+
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/users/login">
-          <Login></Login>
-        </Route>
-        <Route path="/">
-        <Home></Home>
-        </Route>
-      </Switch>
+      <UserProvider>
+        <Switch>
+          <Route path="/users/login">
+            <Login></Login>
+          </Route>
+          <Route path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </UserProvider>
     </Router>
   );
 }
---
+++ frontend/src/context/userContext.jsx
@@ -0,0 +1,14 @@
+import {createContext} from 'react'
+import { useAuth } from '../hooks/useAuth'
+
+export const Context = createContext()
+
+export const UserProvider = ({children}) => {
+    const {login, authenticated} = useAuth
+
+    return (
+        <Context.Provider value={{login, authenticated}}>
+            {children}
+        </Context.Provider>
+    )
+}
