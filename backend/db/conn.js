89dde6d1a2270032d10e0f94aa299498e0b95790
Geovanne <callegarog00@gmail.com>
2022-04-01 08:04:41 -0300

    adding mongodb in the cloud with .env

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose')
 
+const mongodb_conn = () => {
+    if(!process.env.CONN_MONGODB) throw new Error('É necessário um arquivo .env com a conexão do MONGODB')
+    return process.env.CONN_MONGODB
+}
+
 async function main() {
-    await mongoose.connect('mongodb://localhost:27017/journal')
+    await mongoose.connect(mongodb_conn())
     console.log('Conectou ao moongose')
 }
 
-main().catch((err) => console.error(err))
+main().catch((err) => console.log(err))
 
 module.exports = mongoose
