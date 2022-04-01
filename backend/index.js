27b60409749b4ee4d03c419a6238edd2fabf3d1c
Geovanne <callegarog00@gmail.com>
2022-04-01 08:45:32 -0300

    creating procfile and app_url dotenv

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 //Solve CORS
 app.use(cors( {
     credentials: true, 
-    origin: 'http://localhost:3000'
+    origin: process.env.APP_URL
 }));
 
 // Public folder for images 
